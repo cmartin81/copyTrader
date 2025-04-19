188b1ba43cb5d8e96e494123129fb8c6bb711371
Christian Martin <christian@topmartin.com>
2025-04-19 08:59:48 +0200

    changed width of design

--- src/renderer/src/pages/Bots.tsx
+++ src/renderer/src/pages/Bots.tsx
@@ -997,10 +997,10 @@ const Bots: React.FC = () => {
                     ) : (
                       <div className="space-y-2">
                         {account.symbolMappings.map((mapping, index) => (
-                          <div key={index} className="flex items-center space-x-2">
+                          <div key={index} className="flex items-center gap-1">
                             <input
                               type="text"
-                              className="input input-bordered input-sm w-28"
+                              className="input input-bordered input-xs w-20"
                               value={mapping.sourceSymbol}
                               disabled={!mapping.isEditing}
                               onChange={(e) => {
@@ -1014,9 +1014,9 @@ const Bots: React.FC = () => {
                               }}
                               placeholder="Source"
                             />
-                            <span>→</span>
+                            <span className="text-xs">→</span>
                             <select
-                              className="select select-bordered select-sm w-48"
+                              className="select select-bordered select-xs w-full min-w-[100px]"
                               value={mapping.targetSymbolId}
                               disabled={!mapping.isEditing}
                               onChange={(e) => {
@@ -1029,17 +1029,17 @@ const Bots: React.FC = () => {
                                 })
                               }}
                             >
-                              <option value="">Select a symbol</option>
+                              <option value="">Select symbol</option>
                               {account.symbols?.map(symbol => (
                                 <option key={symbol.id} value={symbol.id}>
                                   {symbol.name}
                                 </option>
                               ))}
                             </select>
-                            <span>×</span>
+                            <span className="text-xs">×</span>
                             <input
                               type="number"
-                              className="input input-bordered input-sm w-12"
+                              className="input input-bordered input-xs w-10"
                               value={mapping.multiplier}
                               disabled={!mapping.isEditing}
                               onChange={(e) => {
@@ -1053,35 +1053,33 @@ const Bots: React.FC = () => {
                               }}
                               step="1"
                             />
-                            <div className="space-x-1">
-                              {mapping.isEditing ? (
-                                <button
-                                  className="btn btn-success btn-sm btn-square"
-                                  onClick={() => handleSaveMapping(account.id, index)}
-                                >
-                                  <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
-                                    <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                                  </svg>
-                                </button>
-                              ) : (
-                                <button
-                                  className="btn btn-info btn-sm btn-square"
-                                  onClick={() => handleEditMapping(account.id, index)}
-                                >
-                                  <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
-                                    <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
-                                  </svg>
-                                </button>
-                              )}
+                            {mapping.isEditing ? (
                               <button
-                                className="btn btn-error btn-sm btn-square"
-                                onClick={() => handleDeleteSymbolMapping(account.id, index)}
+                                className="btn btn-success btn-xs btn-square"
+                                onClick={() => handleSaveMapping(account.id, index)}
                               >
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
-                                  <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
+                                  <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                                 </svg>
                               </button>
-                            </div>
+                            ) : (
+                              <button
+                                className="btn btn-ghost btn-xs btn-square"
+                                onClick={() => handleEditMapping(account.id, index)}
+                              >
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
+                                  <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                                </svg>
+                              </button>
+                            )}
+                            <button
+                              className="btn btn-error btn-xs btn-square"
+                              onClick={() => handleDeleteSymbolMapping(account.id, index)}
+                            >
+                              <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
+                                <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+                              </svg>
+                            </button>
                           </div>
                         ))}
                         <button
