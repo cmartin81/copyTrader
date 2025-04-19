ecf4ee0a053f6a26d2f5d954766f265b639a7127
Christian Martin <christian@topmartin.com>
2025-04-19 23:22:12 +0200

    Adjust alert to show on modal

--- src/renderer/src/layouts/MainLayout.tsx
+++ src/renderer/src/layouts/MainLayout.tsx
@@ -240,7 +240,7 @@ const MainLayout = (): JSX.Element => {
         <main className="flex-1 bg-base-200 overflow-y-auto">
           <div className="max-w-7xl">
             {/* Alerts container */}
-            <div className="fixed top-4 right-4 z-50 space-y-2">
+            <div className="fixed top-4 right-4 z-[100] space-y-2">
               {alerts.map((alert) => (
                 <Alert
                   key={alert.id}
